ce7418ef07a8e0470d15fafd299e360972634422
GGWP-grats <ggwork420@gmail.com>
2021-02-07 05:12:33 +0300

    strlcpy correhted to const

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: wquenten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 20:55:53 by wquenten          #+#    #+#             */
-/*   Updated: 2021/02/06 21:26:43 by wquenten         ###   ########.fr       */
+/*   Updated: 2021/02/07 05:12:03 by wquenten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t			ft_strlcpy(char *dst, char *src, size_t size)
+size_t			ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t		i;
 
